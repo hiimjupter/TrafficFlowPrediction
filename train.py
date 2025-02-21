1aa851b467839fc9b901a6d7e52be76d5890a0bf
hilton_easycode <trunghieu.nguyen1107@gmail.com>
2025-02-21 10:12:25 +0700

    fix: relocate models into folder with model name and scats

--- train.py
+++ train.py
@@ -12,11 +12,12 @@ from xgboost import XGBRegressor
 import catboost
 from catboost import CatBoostRegressor
 from sklearn.metrics import mean_squared_error, mean_absolute_percentage_error
-import pickle  # added import
+import pickle
+import os
 warnings.filterwarnings("ignore")
 
 
-def train_model(model, X_train, y_train, name, config):
+def train_model(model, X_train, y_train, scats, name, config):
     """Train a neural network model."""
     model.compile(loss="mse", optimizer="rmsprop", metrics=['mape'])
     hist = model.fit(
@@ -25,25 +26,32 @@ def train_model(model, X_train, y_train, name, config):
         epochs=config["epochs"],
         validation_split=0.05)
 
-    model.save('model/' + name + '.h5')
+    save_location = 'model/{name}/{scats}'.format(name=name, scats=scats)
+
+    if not os.path.exists(save_location):
+        os.makedirs(save_location)
+
+    model.save(save_location + '/model.h5')
     df = pd.DataFrame.from_dict(hist.history)
-    df.to_csv('model/' + name + ' loss.csv', encoding='utf-8', index=False)
+    df.to_csv(save_location + '/model_loss.csv', encoding='utf-8', index=False)
 
 
-def train_ml_model(model, X_train, y_train, name, config):
+def train_ml_model(model, X_train, y_train, scats, name, config):
     """Train a classical ML model such as RandomForest, XGBoost, or CatBoost."""
     model.fit(X_train, y_train)
     y_pred = model.predict(X_train)
     mse = mean_squared_error(y_train, y_pred)
     mape = mean_absolute_percentage_error(y_train, y_pred)
 
+    save_location = 'model/{name}/{scats}'.format(name=name, scats=scats)
+
     # Save model using pickle
-    with open('model/' + name + '.pkl', 'wb') as f:
+    with open(save_location + '/model' + '.pkl', 'wb') as f:
         pickle.dump(model, f)
     print(f"Training completed for {name} with MSE: {mse} and MAPE: {mape}")
 
 
-def train_seas(models, X_train, y_train, name, config):
+def train_seas(models, X_train, y_train, scats, name, config):
     """Train the SAEs model."""
     temp = X_train
     for i in range(len(models) - 1):
@@ -66,7 +74,7 @@ def train_seas(models, X_train, y_train, name, config):
         weights = models[i].get_layer('hidden').get_weights()
         saes.get_layer('hidden%d' % (i + 1)).set_weights(weights)
 
-    train_model(saes, X_train, y_train, name, config)
+    train_model(saes, X_train, y_train, scats, name, config)
 
 
 def main(argv):
@@ -100,26 +108,26 @@ def main(argv):
     if args.model == 'lstm':
         X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
         m = model.get_lstm([12, 64, 64, 1])
-        train_model(m, X_train, y_train, args.model, config)
+        train_model(m, X_train, y_train, scats_num, args.model, config)
     elif args.model == 'gru':
         X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
         m = model.get_gru([12, 64, 64, 1])
-        train_model(m, X_train, y_train, args.model, config)
+        train_model(m, X_train, y_train, scats_num, args.model, config)
     elif args.model == 'saes':
         X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1]))
         m = model.get_saes([12, 400, 400, 400, 1])
-        train_seas(m, X_train, y_train, args.model, config)
+        train_seas(m, X_train, y_train, scats_num, args.model, config)
     elif args.model == 'random_forest':
         m = RandomForestRegressor(n_estimators=100, random_state=42)
-        train_ml_model(m, X_train, y_train, args.model, config)
+        train_ml_model(m, X_train, y_train, scats_num, args.model, config)
     elif args.model == 'xgboost':
         m = XGBRegressor(n_estimators=100, learning_rate=0.1,
                          max_depth=3, random_state=42)
-        train_ml_model(m, X_train, y_train, args.model, config)
+        train_ml_model(m, X_train, y_train, scats_num, args.model, config)
     elif args.model == 'catboost':
         m = CatBoostRegressor(iterations=100, learning_rate=0.1,
                               depth=6, random_state=42, verbose=0)
-        train_ml_model(m, X_train, y_train, args.model, config)
+        train_ml_model(m, X_train, y_train, scats_num, args.model, config)
     else:
         print(f"Model {args.model} is not supported.")
         sys.exit(1)
